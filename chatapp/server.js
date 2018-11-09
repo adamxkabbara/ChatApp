53909f34522dc3e2fe958ef13dd6f9dba3d7c579
adamxkabbara <adamxkabbara@gmail.com>
2018-11-08 17:49:46 -0800

    First commit: add current worked files

---
+++ chatapp/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var httpServer = http.createServer(app);
+
+var myLogStatement = function(req, res, next) {
+    console.log("Received", req.method, "request for resource", req.path,
+    "from", req.ip);
+    next(); // calback to the middleware function
+}
+
+app.use(myLogStatement);
+
+app.use(express.static('src'));
+
+httpServer.listen(3000, function(){
+    console.log("Listening on port 3000");
+});
